d3ffa2c018e34df0eddc85c4aa90a4f10c8f61f7
Martyn M <8057554+martyn-m@users.noreply.github.com>
2020-05-18 17:19:26 +0100

    Chapter 5 Logins

--- app/models.py
+++ app/models.py
@@ -1,18 +1,31 @@
 # defines the app database schema
-from app import db
+from app import db, login
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 # Classes represent a table
-class User(db.Model):
+# Inherits from the base db.Model class and Flask-Login Usermixin
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
     # The __repr__ method defines how instances of this class are printed
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
