4b8f0dd5b8c4f0ebbac295002b7acd2c91e371b4
Martyn M <8057554+martyn-m@users.noreply.github.com>
2020-05-18 15:20:30 +0100

    Ch4 Complete

--- app/models.py
+++ app/models.py
@@ -1,13 +1,25 @@
 # defines the app database schema
 from app import db
+from datetime import datetime
 
 # Classes represent a table
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unqiue=True)
+    username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     # The __repr__ method defines how instances of this class are printed
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    # ForeignKey links this user_id field to the id field in the users table
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
